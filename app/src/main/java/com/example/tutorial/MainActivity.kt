98a125bb3cf96321ebfb913584b2dd6095ce4106
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-08-16 02:25:14 +0530

    minor change

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.tutorial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
